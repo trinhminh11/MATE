62d8b781d090ff82bb52420ba8051a0368d56c32
trinhminh11 <trinhtheminh1104@gmail.com>
2026-03-21 20:27:28 +0700

    up test

--- example.py
+++ example.py
@@ -10,43 +10,52 @@ MAX_EPISODE_STEPS = 4000
 
 
 def main():
+	from copy import deepcopy
+
 	base_env = gym.make('MultiAgentTracking-v0', config = "MATE-4v8-9.yaml")
 
 	env = mate.MultiCamera.make(base_env, target_agent=GreedyTargetAgent())
 
+	env.reset()
 
-	camera_agents = GreedyCameraAgent().spawn(env.unwrapped.num_cameras)
+	env_test_1 = deepcopy(env)
+	env_test_2 = deepcopy(env)
 
-	camera_joint_observation, _ = env.reset()
-
-	mate.group_reset(camera_agents, camera_joint_observation)
-	camera = None
-
-	run = True
-
-	for i in range(MAX_EPISODE_STEPS):
-
-		camera_joint_action = mate.group_step(
-			env.unwrapped, camera_agents, camera_joint_observation, camera
-		)
-
-		results = env.step(camera_joint_action)
-
-		camera_joint_observation, target_team_reward, done, truncated, camera = results
-
-		run = env.render()
-		# arr = env.render(mode='rgb_array')
-
-		# plt.imshow(arr)
-		# plt.show()
-		# arr: np.ndarray
-
-		# a = input()
+	env.reset()
+	env_test_1.reset()
+	env_test_2.reset()
 
 
-		if not run or done:
-			env.close()
-			break
+	print(env.unwrapped.cameras[0].location)
+	print(env_test_1.unwrapped.cameras[0].location)
+	print(env_test_2.unwrapped.cameras[0].location)
+
+
+	# camera_agents = GreedyCameraAgent().spawn(env.unwrapped.num_cameras)
+
+	# camera_joint_observation, _ = env.reset()
+
+	# mate.group_reset(camera_agents, camera_joint_observation)
+	# camera = None
+
+	# run = True
+
+	# for i in range(MAX_EPISODE_STEPS):
+
+	# 	camera_joint_action = mate.group_step(
+	# 		env.unwrapped, camera_agents, camera_joint_observation, camera
+	# 	)
+
+	# 	results = env.step(camera_joint_action)
+
+	# 	camera_joint_observation, target_team_reward, done, truncated, camera = results
+
+	# 	run = env.render()
+
+
+	# 	if not run or done:
+	# 		env.close()
+	# 		break
 
 	# print(t[0][-1, ])
 
